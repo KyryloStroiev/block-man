5d3bf59e557f95bddbb5a2a9cb94feecd445c12c
Kyrylo <stroievkyrylo@gmail.com>
2024-11-07 18:20:25 +0200

    3

--- src/block-man/Assets/Code/Gameplay/Features/Block/Factory/BlockFactory.cs
+++ src/block-man/Assets/Code/Gameplay/Features/Block/Factory/BlockFactory.cs
@@ -15,6 +15,7 @@ namespace Code.Gameplay.Features.Block.Factory
         {
             _identifiers = identifiers;
             _staticDataService = staticDataService;
+            Debug.Log("3");
         }
 
         public GameEntity CreateBlock(BlockTypeId typeId, Vector3 at)
